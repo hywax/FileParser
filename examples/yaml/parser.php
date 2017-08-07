6f28ba0c4a6b0c0813dfdb682fa1db982c90daf0
Alexander Pushkarev <axp-dev@yandex.com>
2017-08-07 14:48:53 +0500

    YAML example refactor

--- examples/yaml/parser.php
+++ examples/yaml/parser.php
@@ -5,9 +5,7 @@ use AXP\FileParser\FileParser;
 include_once '../../vendor/autoload.php';
 
 $file = file_get_contents('./file.yaml', FILE_USE_INCLUDE_PATH);
-$data = FileParser::yaml('latitude: 52.7157856867271
-           longitude: -8.8741735070805
-           zoom: 15');
+$data = FileParser::yaml($file);
 
 
 print_r($data);
